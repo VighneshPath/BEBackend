3b7596bd1a427a965c199f6939b899bc38635fbf
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-25 22:16:16 +0530

    Add cors with allowed origin localhost:3000, Change get-video to GET

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from typing import Union
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from services.GetSignData import predict_class_for_video
 from services.GetVideo import get_video
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -21,7 +34,7 @@ async def create_upload_file(file: UploadFile = File()):
     return {"predicted_class": predict_class_for_video("temp.mp4")}
 
 
-@app.post("/get-video/{text}")
+@app.get("/get-video/{text}")
 async def get_video_from_text(text):
     vids = get_video(text.strip().lower())
     if(vids == []):
